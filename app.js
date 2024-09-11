0a12e0d5a54e41fe80c55a649f2ad9250d976973
Glitch (u-tonomy) <none>
2024-09-11 07:18:49 +0000

    🚥📹 Checkpoint ./config/passportConfig.js:26832867/657 ./app.js:26832867/4111

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require("express");
 const path = require("path");
 const session = require("express-session");
 const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const SQLiteStore = require("connect-sqlite3")(session);
 const db = require("./db/index");
 const authRoutes = require("./routes/authRoutes");
 const postRoutes = require("./routes/postRoutes");
 const groupRoutes = require("./routes/groupRoutes");
-const passportConfig = require("./config/passportConfig");
 
 const app = express();
 const port = 3000;
@@ -31,6 +31,28 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, "public")));
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "https://u-tonomy.glitch.me/auth/google/callback",
+      scope: ["profile", "email"],
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
 
 // Passportの設定を初期化
 passportConfig();
--- config/passportConfig.js
+++ config/passportConfig.js
@@ -1,28 +1,6 @@
 // config/passportConfig.js
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const db = require("../db/index");
 
 module.exports = function() {
-  passport.use(
-    new GoogleStrategy(
-      {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "https://u-tonomy.glitch.me/auth/google/callback",
-        scope: ["profile", "email"],
-      },
-      (accessToken, refreshToken, profile, done) => {
-        return done(null, profile);
-      }
-    )
-  );
-
-  passport.serializeUser((user, done) => {
-    done(null, user);
-  });
-
-  passport.deserializeUser((obj, done) => {
-    done(null, obj);
-  });
 };
