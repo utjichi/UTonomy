0f406d92cc4f88eaaa1167b3fa2d873228ad9e53
Glitch (u-tonomy) <none>
2024-09-19 13:22:36 +0000

    👸🏂 Checkpoint ./public/vote.js:26832867/274

--- public/vote.js
+++ public/vote.js
@@ -1,1 +1,6 @@
-document.getElementsByClassName("addOption")
+document.getElementsByClassName("addOption").forEach(btn=>{
+  btn.onclick=(event)=>{
+    const newOption=document.createElement()
+    document.getElementById(`options${event.target.id.slice("addOption".length)}`).append()
+  }
+})
