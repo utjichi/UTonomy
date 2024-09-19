3255c48a098c4eec855b1eb324f4e7f1c83ab9f2
Glitch (u-tonomy) <none>
2024-09-19 13:33:40 +0000

    👯⛺️ Checkpoint ./public/vote.js:26832867/89

--- public/vote.js
+++ public/vote.js
@@ -1,6 +1,7 @@
 document.getElementsByClassName("addOption").forEach(btn=>{
   btn.onclick=(event)=>{
-    const newOption=document.createElement()
-    document.getElementById(`options${event.target.id.slice("addOption".length)}`).append()
+    const newOption=document.createElement("label")
+    newOption.innerHTML="<input type='radio' name='vote'>"
+    document.getElementById(`options${event.target.id.slice("addOption".length)}`).append(newOption)
   }
 })
